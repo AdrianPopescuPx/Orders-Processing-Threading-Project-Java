ddada905044d4aae0e580e72805674c1bd961d54
adrianpopescupx <adrianpopescupx@gmail.com>
2023-11-20 13:04:31 +0200

    Preparing threads for reading the orders

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static final String inputOrderProducts = "/order_products.txt";
+    public static final String inputOrder = "/order.txt";
+
+    public static final String outputOrderProducts = "order_products_out.txt";
+
+    public static final String outputOrder = "order_out.txt";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Error: Te rog sa furnizezi exact doua argumente!");
+            System.exit(1);
+        }
+
+        String inputLocation = args[0];
+        int threadsNumber = Integer.parseInt(args[1]);
+
+        BufferedReader br = new BufferedReader(new FileReader(inputLocation));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        ExecutorService ordersExecutorService = Executors.newFixedThreadPool(threadsNumber);
+        ExecutorService productsExecutorService = Executors.newFixedThreadPool(threadsNumber);
+
     }
 }
